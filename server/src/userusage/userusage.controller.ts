324d7ab9be66d6dc1f99c5243120a116163b6cbf
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-02-08 12:14:20 +0100

    feat(proto): putting usage in its own service

--- server/src/userusage/userusage.controller.ts
+++ server/src/userusage/userusage.controller.ts
@@ -8,7 +8,7 @@ import { Observable, Subject } from 'rxjs';
 export class UserusageController {
   constructor(private eventEmitter: EventEmitter2) {}
 
-  @GrpcMethod('TaskService')
+  @GrpcMethod('UserService')
   async Using(request: UsageRequest): Promise<UsageResponse> {
     const { username, taskName } = request;
 
@@ -23,7 +23,7 @@ export class UserusageController {
     };
   }
 
-  @GrpcMethod('TaskService')
+  @GrpcMethod('UserService')
   UsingStream(request: UsageRequest): Observable<UsageResponse> {
     try {
       const stream$ = new Subject<UsageResponse>();
