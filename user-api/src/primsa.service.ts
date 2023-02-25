8d0ed11f0a814287d5290e657d4b4d21522cd2d3
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-02-25 22:05:11 +0100

    user-api push

---
+++ user-api/src/primsa.service.ts
@@ -0,0 +1,37 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+    this.$use(async (params, next) => {
+      if (params.model === 'User' && params.action == 'create') {
+        const user = params.args.data;
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(user.password, salt);
+        user.password = hash;
+        params.args.data = user;
+      }
+      return next(params);
+    });
+    this.$use(async (params, next) => {
+      const result = await next(params);
+      if (params?.model === 'User' && params?.args?.select?.password !== true) {
+        delete result.password;
+      }
+      return result;
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
---
+++ user-api/src/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @MessagePattern('createUser')
+  create(@Payload() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @MessagePattern('findAllUser')
+  findAll() {
+    return this.userService.users({});
+  }
+
+  @MessagePattern('findOneUser')
+  findOne(@Payload() id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @MessagePattern('updateUser')
+  update(@Payload() updateUserDto: UpdateUserDto) {
+    return this.userService.update(updateUserDto.id, updateUserDto);
+  }
+
+  @MessagePattern('removeUser')
+  remove(@Payload() id: number) {
+    return this.userService.remove(id);
+  }
+}
