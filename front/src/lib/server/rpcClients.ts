cda4bae10d5089c6b8c2e2f9d1082180a7343240
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-03-11 00:50:26 +0100

    clean ups

---
+++ front/src/lib/server/rpcClients.ts
@@ -0,0 +1,41 @@
+import { GrpcTransport } from '@protobuf-ts/grpc-transport';
+import { ChannelCredentials } from '@grpc/grpc-js';
+import {
+	FieldServiceClient,
+	TaskServiceClient,
+	UsageServiceClient
+} from '$lib/stubs/task/v1beta/task.client';
+import fs from 'fs';
+import { UserServiceClient } from '$src/lib/stubs/user/v1alpha/service.client';
+import { AuthServiceClient } from '$src/lib/stubs/auth/v1alpha/service.client';
+import { env } from '$env/dynamic/private';
+
+const credentials = env.secure
+	? ChannelCredentials.createSsl(
+			fs.readFileSync(env.ROOT_CERT as string),
+			fs.readFileSync(env.FRONT_KEY as string),
+			fs.readFileSync(env.FRONT_CERT as string)
+	  )
+	: ChannelCredentials.createInsecure();
+
+const taskTransport = new GrpcTransport({
+	host: env.TASK_API_URL as string,
+	channelCredentials: credentials
+});
+const userTransport = new GrpcTransport({
+	host: env.USER_API_URL as string,
+	channelCredentials: credentials
+});
+const authTransport = new GrpcTransport({
+	host: env.AUTH_API_URL as string,
+	channelCredentials: credentials
+});
+
+export const taskClients = {
+	crudClient: new TaskServiceClient(taskTransport),
+	fieldClient: new FieldServiceClient(taskTransport),
+	usageClient: new UsageServiceClient(taskTransport)
+};
+
+export const userClient = new UserServiceClient(userTransport);
+export const authClient = new AuthServiceClient(authTransport);
