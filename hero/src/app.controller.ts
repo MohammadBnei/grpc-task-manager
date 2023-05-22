981d83f1a1b4912eb5d87c36a102feb8511c46f3
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-05-22 16:29:07 +0200

    feat(hero): add support for gRPC microservices feat(hero): add support for protobuf files feat(hero): add hero CRUD service The changes in `nest-cli.json` add support for protobuf files by including them in the assets array and enabling watchAssets. The changes in `package.json` update the dependencies and devDependencies to their latest versions. The changes in `app.controller.ts` add a new gRPC method for adding a hero to the list of heroes. The changes in `main.ts` add support for gRPC microservices by creating a microservice using the `createMicroservice` method and specifying the transport, options, and package. The `protoPath` option specifies the path to the protobuf file.

--- hero/src/app.controller.ts
+++ hero/src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  AddRequest,
+  AddResponse,
+  HERO_CR_UD_SERVICE_NAME,
+  Hero,
+} from './stubs/hero/v1alpha/hero';
+import { GrpcMethod } from '@nestjs/microservices';
+
+const heroes: Hero[] = [];
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @GrpcMethod(HERO_CR_UD_SERVICE_NAME)
+  async add(request: AddRequest): Promise<AddResponse> {
+    const hero = {
+      ...request,
+      id: heroes.length + 1,
+      hp: 100,
+    };
+
+    heroes.push(hero);
+
+    return {
+      hero,
+    };
   }
 }
--- hero/src/main.ts
+++ hero/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { HERO_V1ALPHA_PACKAGE_NAME } from './stubs/hero/v1alpha/hero';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:6000',
+        package: HERO_V1ALPHA_PACKAGE_NAME,
+        protoPath: join(__dirname, 'proto/hero/v1alpha/hero.proto'),
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
