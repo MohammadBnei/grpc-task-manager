9f8e574e7220b890f79626a6ce85acbdd869c485
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-05-22 17:48:17 +0200

    feat(app.service.ts): add CRUD operations for Hero entity This commit adds the following CRUD operations for the Hero entity: - create: creates a new hero with an id, hp, and other properties specified in the data parameter - findAll: returns an array of all heroes - findById: returns a hero with the specified id - findByName: returns a hero with the specified name - update: updates a hero with the specified id with the properties specified in the data parameter - delete: deletes a hero with the specified id from the heroes array and returns the deleted hero. If the hero is not found, an error is thrown.

--- hero/src/app.service.ts
+++ hero/src/app.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { Hero } from './stubs/hero/v1alpha/hero';
+
+const heroes: Hero[] = [];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  create(data: any): Hero {
+    const hero = {
+      ...data,
+      id: heroes.length + 1,
+      hp: 100,
+    };
+
+    heroes.push(hero);
+
+    return hero;
+  }
+
+  findAll(): Hero[] {
+    return heroes;
+  }
+
+  findById(id: number): Hero {
+    return heroes.find((hero) => hero.id === id);
+  }
+
+  findByName(name: string): Hero {
+    return heroes.find((hero) => hero.name === name);
+  }
+
+  update(id: number, data: any): Hero {
+    const index = heroes.findIndex((hero) => hero.id === id);
+
+    if (index === -1) {
+      throw new Error(`Hero ${id} not found`);
+    }
+
+    heroes[index] = {
+      ...heroes[index],
+      ...data,
+    };
+
+    return heroes[index];
+  }
+
+  delete(id: number): Hero {
+    const index = heroes.findIndex((hero) => hero.id === id);
+
+    if (index === -1) {
+      throw new Error(`Hero ${id} not found`);
+    }
+
+    const deletedHero = heroes.splice(index, 1)[0];
+
+    return deletedHero;
   }
 }
