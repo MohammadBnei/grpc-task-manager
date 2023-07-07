561ad15e1d40cb2ff4c5e7d095efe78f15f84f78
Louis Defaut <loudefaut02@outlook.fr>
2023-07-07 11:41:30 +0200

    feat: new ms

--- docker/src/grpc.option.ts
+++ docker/src/grpc.option.ts
@@ -1,26 +1,26 @@
-import { ConfigService } from '@nestjs/config';
-import { GrpcOptions, Transport } from '@nestjs/microservices';
-import { addReflectionToGrpcConfig } from 'nestjs-grpc-reflection';
-import { join } from 'path';
+import {ConfigService} from '@nestjs/config';
+import {GrpcOptions, Transport} from '@nestjs/microservices';
+import {addReflectionToGrpcConfig} from 'nestjs-grpc-reflection';
+import {join} from 'path';
 
 export default (cs: ConfigService): GrpcOptions =>
-  addReflectionToGrpcConfig({
-    transport: Transport.GRPC,
-    options: {
-      url: '0.0.0.0:' + cs.get<number>('PORT'),
-      package: 'hero.v1alpha',
-      protoPath: join(__dirname, 'hero/hero.proto'),
-    },
-  });
+    addReflectionToGrpcConfig({
+        transport: Transport.GRPC,
+        options: {
+            url: '0.0.0.0:' + cs.get<number>('PORT'),
+            package: 'hero.v1alpha',
+            protoPath: join(__dirname, 'timesheet/timesheet.proto'),
+        },
+    });
 
 export const authGrpcOption = (cs: ConfigService): GrpcOptions => ({
-  transport: Transport.GRPC,
-  options: {
-    url: cs.get('AUTH_URL'),
-    package: 'auth.v1alpha',
-    loader: {
-      includeDirs: [join(__dirname, 'proto')],
+    transport: Transport.GRPC,
+    options: {
+        url: cs.get('AUTH_URL'),
+        package: 'auth.v1alpha',
+        loader: {
+            includeDirs: [join(__dirname, 'proto')],
+        },
+        protoPath: join(__dirname, 'proto/auth/v1alpha/service.proto'),
     },
-    protoPath: join(__dirname, 'proto/auth/v1alpha/service.proto'),
-  },
 });
